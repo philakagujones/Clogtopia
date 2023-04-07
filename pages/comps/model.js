7dcb66967764da792a7277fa2ce4a45a1dcb7b0a
philakagujones <philakagujones@gmail.com>
2023-04-06 23:29:01 -0700

    modern revamp

--- pages/comps/model.js
+++ pages/comps/model.js
@@ -12,7 +12,78 @@ export default function Model() {
   };
   return (
     <> 
-    <div className={styles.tabs}>
+     <div className="content-tabs">
+            <div
+            className={ toggleState === 1 ? `${styles.selectedcontent}` : `${styles.content}` }
+            >
+            <model-viewer src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggywhite.glb?v=1680423840319"
+                  ios-src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggywhite.glb?v=1680423840319"
+                  poster="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/whiteposter.webp?v=1680426640449"
+                  alt="Our beautiful case"
+                  shadow-intensity="1"
+                  camera-controls
+                  auto-rotate 
+                  ar ar-modes="webxr scene-viewer quick-look">
+             </model-viewer>
+            </div>
+
+            <div
+            className={ toggleState === 2 ? `${styles.selectedcontent}` : `${styles.content}` }
+            >
+                <model-viewer src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggyblack.glb?v=1680423829401"
+                  ios-src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggyblack.glb?v=1680423829401"
+                  poster="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/blackposter.webp?v=1680426640921"
+                  alt="Our beautiful case"
+                  shadow-intensity="1"
+                  camera-controls
+                  auto-rotate 
+                  ar ar-modes="webxr scene-viewer quick-look">
+                </model-viewer>
+            </div>
+
+            <div
+            className={ toggleState === 3 ? `${styles.selectedcontent}` : `${styles.content}` }
+            >
+            <model-viewer src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggypurp.glb?v=1680423855023"
+                  ios-src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggypurp.glb?v=1680423855023"
+                  poster="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/purpposter.webp?v=1680426642412"
+                  alt="Our beautiful case"
+                  shadow-intensity="1"
+                  camera-controls
+                  auto-rotate 
+                  ar ar-modes="webxr scene-viewer quick-look">
+             </model-viewer>
+            </div>
+
+            <div
+            className={ toggleState === 4 ? `${styles.selectedcontent}` : `${styles.content}` }
+            >
+            <model-viewer src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggypink.glb?v=1680423831902"
+                  ios-src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggypink.glb?v=1680423831902"
+                  poster="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/pinkposter.webp?v=1680426641861"
+                  alt="Our beautiful case"
+                  shadow-intensity="1"
+                  camera-controls
+                  auto-rotate 
+                  ar ar-modes="webxr scene-viewer quick-look">
+             </model-viewer>
+            </div>
+            <div
+            className={ toggleState === 5 ? `${styles.selectedcontent}` : `${styles.content}` }
+            >
+            <model-viewer src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggygrey.glb?v=1680423862559"
+                  ios-src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggygrey.glb?v=1680423862559"
+                  poster="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/greyposter.webp?v=1680426641383"
+                  alt="Our beautiful case"
+                  shadow-intensity="1"
+                  camera-controls
+                  auto-rotate 
+                  ar ar-modes="webxr scene-viewer quick-look">
+            </model-viewer>
+            </div>
+        </div>
+
+        <div className={styles.tabs}>
             <div className={ toggleState === 1 ? `${styles.selectedtab}` : `${styles.tab}` }
             onClick={() => toggleTab(1)}
             >
@@ -39,72 +110,6 @@ export default function Model() {
                 <p>Grey</p>
             </div>
      </div>
-
-     <div className="content-tabs">
-            <div
-            className={ toggleState === 1 ? `${styles.selectedcontent}` : `${styles.content}` }
-            >
-            <model-viewer src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggywhite.glb?v=1680423840319"
-                  ios-src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggywhite.glb?v=1680423840319"
-                  poster="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/whiteposter.webp?v=1680426640449"
-                  alt="Our beautiful case"
-                  shadow-intensity="1"
-                  camera-controls
-                  auto-rotate ar>
-             </model-viewer>
-            </div>
-
-            <div
-            className={ toggleState === 2 ? `${styles.selectedcontent}` : `${styles.content}` }
-            >
-                <model-viewer src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggyblack.glb?v=1680423829401"
-                  ios-src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggyblack.glb?v=1680423829401"
-                  poster="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/blackposter.webp?v=1680426640921"
-                  alt="Our beautiful case"
-                  shadow-intensity="1"
-                  camera-controls
-                  auto-rotate ar>
-                </model-viewer>
-            </div>
-
-            <div
-            className={ toggleState === 3 ? `${styles.selectedcontent}` : `${styles.content}` }
-            >
-            <model-viewer src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggypurp.glb?v=1680423855023"
-                  ios-src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggypurp.glb?v=1680423855023"
-                  poster="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/purpposter.webp?v=1680426642412"
-                  alt="Our beautiful case"
-                  shadow-intensity="1"
-                  camera-controls
-                  auto-rotate ar>
-             </model-viewer>
-            </div>
-
-            <div
-            className={ toggleState === 4 ? `${styles.selectedcontent}` : `${styles.content}` }
-            >
-            <model-viewer src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggypink.glb?v=1680423831902"
-                  ios-src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggypink.glb?v=1680423831902"
-                  poster="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/pinkposter.webp?v=1680426641861"
-                  alt="Our beautiful case"
-                  shadow-intensity="1"
-                  camera-controls
-                  auto-rotate ar>
-             </model-viewer>
-            </div>
-            <div
-            className={ toggleState === 5 ? `${styles.selectedcontent}` : `${styles.content}` }
-            >
-            <model-viewer src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggygrey.glb?v=1680423862559"
-                  ios-src="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/cloggygrey.glb?v=1680423862559"
-                  poster="https://cdn.glitch.global/acfaa7f2-487e-4b9d-a891-abed8d51d319/greyposter.webp?v=1680426641383"
-                  alt="Our beautiful case"
-                  shadow-intensity="1"
-                  camera-controls
-                  auto-rotate ar>
-            </model-viewer>
-            </div>
-        </div>
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Marquee from './comps/marquee'
 import { Text, Button } from "@nextui-org/react";
 import dynamic from 'next/dynamic'
 import Footer from './comps/footer'
+import Background from '@/images/cloggybackground.png'
 
 const Model = dynamic(
   () => import('./comps/model'),
@@ -19,42 +20,77 @@ export default function Home() {
         <title>Clogtopia</title>
         <meta name="description" content="Clogtopia is home to the greatest phone cases and charms in existence." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#FFFFFF" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Marquee text="LIMITED TIME - 20% OFF TWO CASES ON KICKSTARTER &nbsp; &nbsp; &nbsp; &nbsp; LET'S GET CLOGGY &nbsp; &nbsp; &nbsp; &nbsp;"/>
-      <Navski />
-      <main className={styles.main}>
-        <div className={styles.words}>
-          <Text
-            h1
-            size={45}
-            css={{
-              textGradient: "45deg, $green600 -20%, $blue600 50%",
-            }}
-            weight="bold"
-          >
-            The only iPhone case you will ever need.
-          </Text>
-        </div>
-          <div className={styles.animation}>
-            <Model />
-          </div>
-          <div className={styles.button}>
-            <Button shadow color="gradient">
-              Kickstarter Campaign
-            </Button>
-          </div>
-          <div className={styles.words}>
-          <Text
-            h1
-            size={45}
-            color="white"
-            weight="bold"
-          >
-            Stay tuned.
-          </Text>
-        </div>
-      </main>
+      <div className={styles.sticky}>
+          <Marquee text="LIMITED TIME - EARLY BIRD OFFER ON KICKSTARTER COMING SOON &nbsp; &nbsp; &nbsp; &nbsp; LET'S GET CLOGGY &nbsp; &nbsp; &nbsp; &nbsp;"/>
+          <Navski />
+      </div>
+      <div className={styles.scroller}>
+           <section className={styles.child}>
+            <div className={styles.top}>
+              <div className={styles.wordstop}>
+                <Text
+                    h1
+                    size={45}
+                    css={{
+                      textGradient: "45deg, $green600 -20%, $blue600 50%",
+                    }}
+                    weight="bold"
+                  >
+                    The only iPhone case you will ever need.
+                </Text>
+                <div className={styles.cloggy}>
+                  <Image 
+                    src={Background}
+                    alt="Cloggy photo"
+                    style={{
+                      marginLeft: "15px", marginTop: "7rem"
+                    }}
+                    height={400}
+                  />
+                </div>
+              </div>     
+            </div>      
+           </section>
+           <section className={styles.child}>
+              <div className={styles.animation}>
+                <div className={styles.spacerwords}>
+                    <Text
+                      h1
+                      size={45}
+                      css={{
+                        textGradient: "45deg, $green600 -20%, $blue600 50%",
+                      }}
+                      weight="bold"
+                    >
+                    Give &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; a try.
+                  </Text>
+                </div>
+                <Model />
+              </div>           
+           </section>
+           <section className={styles.child}>
+           <div className={styles.button}>
+                <Button shadow color="gradient">
+                  Kickstarter Campaign
+                </Button>
+              </div>
+              <div className={styles.words}>
+              <Text
+                h1
+                size={45}
+                color="black"
+                weight="bold"
+              >
+                Stay tuned.
+              </Text>
+            </div>           
+        </section>
+      </div>
+      {/* <main className={styles.main}>
+      </main> */}
       {/* <Footer /> */}
     </>
   )
