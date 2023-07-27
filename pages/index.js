22a021e198d7596c1abd8945483883699105fd5e
philakagujones <philakagujones@gmail.com>
2023-07-27 12:22:21 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,19 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#FFFFFF" />
         <link rel="icon" href="/favicon.ico" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,h,i,m,p){
+                m=c.createElement(h);
+                p=c.getElementsByTagName(h)[0];
+                m.async=1;
+                m.src=i;
+                p.parentNode.insertBefore(m,p);
+              })(document,"script","https://chimpstatic.com/mcjs-connected/js/users/ff287ff9b3f44b575ca9256cc/c5ff3089c732d5ca82a4cab05.js");
+            `,
+          }}
+        />
       </Head>
       <div className={styles.sticky}>
           <Marquee text="EARLY BIRD PREORDERS AVAILABLE NOW &nbsp; &nbsp; &nbsp; &nbsp; FREE NA SHIPPING ON ORDERS $100+ &nbsp; &nbsp; &nbsp; &nbsp;"/>
